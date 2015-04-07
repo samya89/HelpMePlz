2b3b8a0dd9b2c93c61a3107d15b6323fda859307
Shahin Zangenehpour <98chimp@users.noreply.github.com>
2015-04-07 16:53:04 -0700

    push notification set up; ape faces added

--- HelpMePlz/AppDelegate.m
+++ HelpMePlz/AppDelegate.m
@@ -22,53 +22,53 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    [Parse setApplicationId:@"hA5hkShzgSnPP3gjZZx9Vu0DdS5xarIeT8QBLKI9" clientKey:@"TYGLDSmIfxtdAFsGPOmG7VEiaWoBdpUXqhdumg4i"];
-//    [PFFacebookUtils initializeFacebook];
-    [PFTwitterUtils initializeWithConsumerKey:@"LYyqXWNcR1dUvQAWmJSC4zmwK" consumerSecret:@"bdSzENzQgLkgEgUh2xWRsoogfl4355sac526inZXuRuUt3iahu"];
+    [Parse setApplicationId:@"hA5hkShzgSnPP3gjZZx9Vu0DdS5xarIeT8QBLKI9"
+                  clientKey:@"TYGLDSmIfxtdAFsGPOmG7VEiaWoBdpUXqhdumg4i"];
+    
+    [PFTwitterUtils initializeWithConsumerKey:@"LYyqXWNcR1dUvQAWmJSC4zmwK"
+                               consumerSecret:@"bdSzENzQgLkgEgUh2xWRsoogfl4355sac526inZXuRuUt3iahu"];
     
     // Set default ACLs
     PFACL *defaultACL = [PFACL ACL];
     [defaultACL setPublicReadAccess:YES];
     [PFACL setDefaultACL:defaultACL withAccessForCurrentUser:YES];
     
-//    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[DemoTableViewController alloc] init]];
-//    self.window.backgroundColor = [UIColor whiteColor];
-//    [self.window makeKeyAndVisible];
+    UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
+                                                    UIUserNotificationTypeBadge |
+                                                    UIUserNotificationTypeSound);
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
+                                                                             categories:nil];
+    [application registerUserNotificationSettings:settings];
+    [application registerForRemoteNotifications];
 
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [FBSession.activeSession handleDidBecomeActive];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
-// Facebook oauth callback
-//- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
-//    return [PFFacebookUtils handleOpenURL:url];
-//}
-//
-//- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
-//  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
-//    return [PFFacebookUtils handleOpenURL:url];
-//}
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    // Store the deviceToken in the current installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    currentInstallation.channels = @[ @"global" ];
+    [currentInstallation saveInBackground];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [PFPush handlePush:userInfo];
+}
 
 @end
