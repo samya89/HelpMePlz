bc6b0d3c7a342dbdea8997834b93e386ac56d5e2
Shahin Zangenehpour <98chimp@users.noreply.github.com>
2015-04-09 11:27:39 -0700

    got archives class working, yay!

--- HelpMePlz/DetailHelpStudentViewController.m
+++ HelpMePlz/DetailHelpStudentViewController.m
@@ -10,6 +10,8 @@
 
 @interface DetailHelpStudentViewController ()
 
+@property (assign, nonatomic) BOOL issueResolved;
+
 @end
 
 @implementation DetailHelpStudentViewController
@@ -45,14 +47,50 @@
 }
 
 - (IBAction)startAssisting:(UIButton *)sender{
-    
 }
 
 //BOOL issueResolved = YES;
 //    [request setObject:[NSNumber numberWithBool:issueResolved] forKey:@"issueResolved"];
 
+- (IBAction)issueResolvedSwitch:(UISwitch *)sender {
+    if ([sender isOn]) {
+        self.issueResolved = YES;
+    }
+    else {
+        self.issueResolved = NO;
+    }
+
+}
 
 - (IBAction)submitAssistDetails:(UIButton *)sender{
+    
+    NSString *name = [PFUser currentUser][@"Name"];
+    NSString *notes = @"add notes here";
+    NSTimeInterval helpDuration = 0;
+    
+    PFObject *archive = [PFObject objectWithClassName:@"Archives"];
+    [archive setObject:name forKey:@"name"];
+    [archive setObject:notes forKey:@"notes"];
+    
+    [archive setObject:[NSNumber numberWithBool:self.issueResolved] forKey:@"issueResolved"];
+    [archive setObject:[NSNumber numberWithDouble:helpDuration] forKey:@"helpDuration"];
+    
+    [archive setObject:[[PFUser currentUser] objectId] forKey:@"senderId"];
+    [archive setObject:[[PFUser currentUser] username] forKey:@"senderName"];
+    [archive saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (error) {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"An error occurred!"
+                                                                message:@"Please try sending your message again."
+                                                               delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+        }
+        else {
+            // Everything was successful!
+            NSLog(@"Request saved successfully, yay!");
+        }
+    }];
+
+    /*
     PFQuery *query = [PFQuery queryWithClassName:@"Requests"];
     
     [query whereKey:@"notes" equalTo:@"add notes here"];
@@ -72,6 +110,7 @@
             [requestItem saveInBackground];
     }];
     //     [self dismissViewControllerAnimated:YES completion:nil];
+     */
 }
 
 
